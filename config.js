fa2ca647ef79c7e1f5a699e9b0eb9bef4c49bbb0
sehanthilakarathna <www.sehanthilakarathna@gmail.com>
2024-10-14 03:04:03 -0700

    Update config.js
    Signed-off-by: sehanthilakarathna <www.sehanthilakarathna@gmail.com>

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
-}
+}trou
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "mHRQUZ6C#pGUVCZOsrhomWZgVbMJpn8HvXnuLhYlhuA4yWx61aAc",
 MONGODB: process.env.MONGODB || "",
